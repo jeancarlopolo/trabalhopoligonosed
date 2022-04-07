8ea498b602c7ece0bab570976a4c3e6cf30c01f3
Jean Carlo <92150848+jeancarlopolo@users.noreply.github.com>
2022-04-07 11:58:47 -0300

    Update retangulo.c

--- jeancp/retangulo.c
+++ jeancp/retangulo.c
@@ -1,15 +1,17 @@
+#include "retangulo.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 typedef struct
 {
   int index;
   double x, y, w, h;
   char corb[30], corp[30];
-} Retangulo;
-Retangulo *criarretangulo(int index, double x, double y, double w, double h, char *corb, char *corp)
+} RetStruct;
+Rectangle criarretangulo(int index, double x, double y, double w, double h, char *corb, char *corp)
 {
-  Retangulo *p = malloc(sizeof(Retangulo));
+  RetStruct *p = malloc(sizeof(Retangulo));
   p->index = index;
   p->x = x;
   p->y = y;
@@ -18,4 +20,15 @@ Retangulo *criarretangulo(int index, double x, double y, double w, double h, cha
   strcpy(p->corb, corb);
   strcpy(p->corp, corp);
   return p;
+}
+void printdados(Rectangle r)
+{
+    RetStruct* ret = (Retangulo*) r;
+    printf("%lf", ret->x);
+}
+void main()
+{
+    Rectangle p = criarretangulo(1, 2, 3, 4, 5, "white", "grey");
+    setx(p, getx(p) + 2);
+    printdados(p);
 }
