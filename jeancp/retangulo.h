23e2b52ac81c58db2523653d8ae72e3d39cdc1c8
Jean Carlo <92150848+jeancarlopolo@users.noreply.github.com>
2022-04-07 11:59:28 -0300

    Update retangulo.h

--- jeancp/retangulo.h
+++ jeancp/retangulo.h
@@ -1,1 +1,9 @@
+#ifndef __RETANGULO_H_
+#define __RETANGULO_H_
 
+
+typedef void* Rectangle;
+
+Rectangle criarretangulo(int index, double x, double y, double w, double h, char *corb, char *corp);
+
+#endif
